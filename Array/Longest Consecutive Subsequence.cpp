1e61b9dc20ae6215ad15885d42fc0c2a8122ba20
Narsi <154037409+Narsi7777@users.noreply.github.com>
2025-03-11 21:04:13 +0530

    Create Longest Consecutive Subsequence.cpp

---
+++ Array/Longest Consecutive Subsequence.cpp
@@ -0,0 +1,30 @@
+
+// https://www.geeksforgeeks.org/problems/longest-consecutive-subsequence2449/1
+
+class Solution {
+  public:
+
+    // Function to return length of longest subsequence of consecutive integers.
+    int longestConsecutive(vector<int>& arr) {
+        int n=arr.size();
+        
+        unordered_set<int> st;
+        int res=0;
+        for(int a:arr){
+            st.insert(a);
+        }
+        for(int a:arr){
+            if(st.find(a)!=st.end()&&st.find(a-1)==st.end()){
+                int curr=a;
+                int count=0;
+               while(st.find(curr)!=st.end()){
+                   count++;
+                   st.erase(curr);
+                   curr++;
+               } 
+               res=max(res,count);
+            }
+        }
+        return res;
+    }
+};
